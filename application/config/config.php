18aa2b8f7bc2c790595460d6143b425a3dd16cc3
Andrew Wilson <andrew.wilsonx@gmail.com>
2013-01-20 00:48:55 -0800

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8888/codeigniter/';
+$config['base_url']	= 'http://localhost:8888/ajw/';
 
 /*
 |--------------------------------------------------------------------------
