86d8c0f69900205eb2215a3e0c076d00bfb0e457
Andrew Wilson <andrew.wilsonx@gmail.com>
2013-01-25 13:46:48 -0800

    removed redundancy
    added class properties and moved cURL setops to the constructor

--- application/models/yummly_model.php
+++ application/models/yummly_model.php
@@ -2,70 +2,57 @@
 
 class Yummly_model extends CI_Model {
 
+    private $APP_ID = 'b35c29b8';
+    private $APP_KEY = '0bc74fba14edec1fedd54cad955961ac';
+    private $BASE_URL = 'http://api.yummly.com/v1/api/';
     
-    function __construct() {
+    private $ch;
+    
+    public function __construct() {
+        
         parent::__construct();
         
-
+        // initialize cURL session
+        $this->ch = curl_init();
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($this->ch, CURLOPT_MAXREDIRS, 20);
+        curl_setopt($this->ch, CURLOPT_ENCODING, "gzip");
+        curl_setopt($this->ch, CURLOPT_HTTPHEADER, array(
+            'X-Yummly-App-ID:'.$this->APP_ID,
+            'X-Yummly-App-Key:'.$this->APP_KEY));
+        
     }
     
     public function search_recipes() {
 
-        $APP_ID = 'b35c29b8';
-        $APP_KEY = '0bc74fba14edec1fedd54cad955961ac';
-        
-        // initialize cURL session
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
-        curl_setopt($ch, CURLOPT_ENCODING, "gzip");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'X-Yummly-App-ID:'.$APP_ID,
-            'X-Yummly-App-Key:'.$APP_KEY));
-        
-        $BASE_URL = 'http://api.yummly.com/v1/api/';
         $search_phrase = 'recipes?q=' . urlencode($this->input->post('search_phrase')) . '&maxResult=10';
         
-        curl_setopt($ch, CURLOPT_URL, ($BASE_URL . $search_phrase));
+        curl_setopt($this->ch, CURLOPT_URL, ($this->BASE_URL . $search_phrase));
         
         // decoded json data
-        $decoded_json_data = json_decode(curl_exec($ch), true);
-        curl_close($ch);
+        $decoded_json_data = json_decode(curl_exec($this->ch), true);
+        curl_close($this->ch);
         
         return $decoded_json_data;
-        
         
     }
     
     public function get_recipe($recipe_id) {
-        
-        $APP_ID = 'b35c29b8';
-        $APP_KEY = '0bc74fba14edec1fedd54cad955961ac';
-        
-        // initialize cURL session
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
-        curl_setopt($ch, CURLOPT_ENCODING, "gzip");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'X-Yummly-App-ID:'.$APP_ID,
-            'X-Yummly-App-Key:'.$APP_KEY));
-        
-        $BASE_URL = 'http://api.yummly.com/v1/api/';
+
         $search_phrase = 'recipe/' . $recipe_id;
         
-        curl_setopt($ch, CURLOPT_URL, ($BASE_URL . $search_phrase));
+        curl_setopt($this->ch, CURLOPT_URL, ($this->BASE_URL . $search_phrase));
         
         // decoded json data
-        $decoded_json_data = json_decode(curl_exec($ch), true);
-        curl_close($ch);
+        $decoded_json_data = json_decode(curl_exec($this->ch), true);
+        curl_close($this->ch);
         
         return $decoded_json_data;
         
     }
     
 }
+/* End of file yummly_model.php */
+/* Location: ./application/models/yummly_model.php */
