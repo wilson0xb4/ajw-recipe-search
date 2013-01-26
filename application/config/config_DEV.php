763f943b5095b5da97222efcf9882e32afac531a
Andrew Wilson <andrew.wilsonx@gmail.com>
2013-01-26 13:45:24 -0800

    development version

--- application/config/config_DEV.php
+++ application/config/config_DEV.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8888/ajw/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
